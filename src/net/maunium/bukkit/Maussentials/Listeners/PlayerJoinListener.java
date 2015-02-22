eab893afc01a59fd331340159c285a7cf473f827
Tulir293 <tulir293@maunium.net>
2015-02-22 19:24:52 +0200

    Added comments

--- src/net/maunium/bukkit/Maussentials/Listeners/PlayerJoinListener.java
+++ src/net/maunium/bukkit/Maussentials/Listeners/PlayerJoinListener.java
@@ -10,6 +10,10 @@ import net.maunium.bukkit.Maussentials.Maussentials;
 import net.maunium.bukkit.Maussentials.Utils.ChatFormatter;
 import net.maunium.bukkit.Maussentials.Utils.MauModule;
 
+/**
+ * @author Tulir293
+ * @since 0.1
+ */
 public class PlayerJoinListener implements Listener, MauModule {
 	private Maussentials plugin;
 	private String[] welcome;
@@ -18,20 +22,26 @@ public class PlayerJoinListener implements Listener, MauModule {
 	public void initialize(Maussentials plugin) {
 		this.plugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		// The reload method contains the loading code so it can be used for loading too.
 		reload();
 	}
 	
 	@Override
 	public void reload() {
+		// Load the list of lines to be in the welcome message from config.
 		List<String> s = plugin.getConfig().getStringList("welcome-message");
+		// Initialize the welcome message array
 		welcome = new String[s.size()];
 		int i = 0;
+		// Loop through the lines
 		for (String ss : s)
+			// Translate the alt format codes to real format codes and add the string to the array.
 			welcome[i] = ChatFormatter.translateAll(ss);
 	}
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent evt) {
+		// Send every line in the welcome message array.
 		evt.getPlayer().sendMessage(welcome);
 	}
 }
