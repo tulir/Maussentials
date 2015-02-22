1f585867015bf3c11b9b36e5d668181166c852b2
Tulir293 <tulir293@maunium.net>
2015-02-22 19:40:18 +0200

    Added login listener for the PlayerData system

---
+++ src/net/maunium/bukkit/Maussentials/Listeners/PlayerLoginListener.java
@@ -0,0 +1,37 @@
+package net.maunium.bukkit.Maussentials.Listeners;
+
+import java.sql.SQLException;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+import net.maunium.bukkit.Maussentials.Maussentials;
+import net.maunium.bukkit.Maussentials.Utils.MauModule;
+
+/**
+ * @author Tulir293
+ * @since 0.1
+ */
+public class PlayerLoginListener implements Listener, MauModule {
+	private Maussentials plugin;
+	
+	@Override
+	public void initialize(Maussentials plugin) {
+		this.plugin = plugin;
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+	
+	@Override
+	public void reload() {}
+	
+	@EventHandler
+	public void onPlayerLogin(PlayerLoginEvent evt) {
+		try {
+			plugin.getMauData().setEntry(evt.getPlayer().getUniqueId(), evt.getPlayer().getName(), evt.getAddress().getHostAddress(),
+					evt.getPlayer().getLocation());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
