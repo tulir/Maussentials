603e04a63d362b373e6ecf4f6091d893145c4f8e
Tulir293 <tulir293@maunium.net>
2015-02-22 17:06:24 +0200

    Added javadoc comments

--- src/net/maunium/bukkit/Maussentials/MauModule.java
+++ src/net/maunium/bukkit/Maussentials/MauModule.java
@@ -1,6 +1,21 @@
 package net.maunium.bukkit.Maussentials;
 
+/**
+ * The base of every module in Maussentials.
+ * 
+ * @author Tulir293
+ * @since 0.1
+ */
 public interface MauModule {
+	/**
+	 * Reloads all the configurations and everything else that needs reloading.
+	 */
 	public void reload();
+	
+	/**
+	 * Initializes the module and loads all the configurations and everything else that needs to be loaded.
+	 * 
+	 * @param plugin The hosting instance of Maussentials.
+	 */
 	public void initialize(Maussentials plugin);
 }
