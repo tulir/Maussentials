61651398b81ffe4646ae4d50864a98b0387ec0f9
Tulir293 <tulir293@maunium.net>
2015-02-22 10:27:25 +0200

    Moved database initialization to own method.

--- src/net/maunium/bukkit/Maussentials/Maussentials.java
+++ src/net/maunium/bukkit/Maussentials/Maussentials.java
@@ -27,7 +27,7 @@ public class Maussentials extends JavaPlugin {
 	public String version;
 	public final String name = "Maussentials", author = "Tulir293", stag = ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + name + ChatColor.DARK_GREEN + "] "
 			+ ChatColor.GRAY, errtag = ChatColor.DARK_RED + "[" + ChatColor.RED + name + ChatColor.DARK_RED + "] " + ChatColor.RED;
-	private PlayerData pd;
+	private MauData md;
 	
 	@Override
 	public void onEnable() {
@@ -35,73 +35,7 @@ public class Maussentials extends JavaPlugin {
 		version = this.getDescription().getVersion();
 		this.saveDefaultConfig();
 		
-		switch (this.getConfig().getString("sql.type").toLowerCase(Locale.ENGLISH)) {
-			case "mysql":
-				pd = new PlayerData(new MySQL(Logger.getLogger("Minecraft"), "Maussentials", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "sqlite":
-				pd = new PlayerData(new SQLite(Logger.getLogger("Minecraft"), "Maussentials", this.getDataFolder().getPath(), getConfig().getString("sql.database")));
-				break;
-			case "h2":
-				pd = new PlayerData(new H2(Logger.getLogger("Minecraft"), "Maussentials", this.getDataFolder().getPath(), getConfig().getString("sql.database")));
-				break;
-			case "postgresql":
-				pd = new PlayerData(new PostgreSQL(Logger.getLogger("Minecraft"), "Maussentials", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "microsoftsql":
-			case "mssql":
-				try {
-					pd = new PlayerData(new MicrosoftSQL(Logger.getLogger("Minecraft"), "Maussentials", getConfig().getString("sql.hostname"), getConfig().getInt(
-							"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				} catch (SQLException e) {
-					getLogger().severe("Failed to initialize MicrosoftSQL connection");
-					e.printStackTrace();
-				}
-				break;
-			case "oracle":
-				try {
-					pd = new PlayerData(new Oracle(Logger.getLogger("Minecraft"), "Maussentials", getConfig().getString("sql.hostname"), getConfig().getInt(
-							"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				} catch (SQLException e) {
-					getLogger().severe("Failed to initialize Oracle database connection");
-					e.printStackTrace();
-				}
-				break;
-			case "ingres":
-				pd = new PlayerData(new Ingres(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "informix":
-				pd = new PlayerData(new Informix(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "db2":
-				pd = new PlayerData(new DB2(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "frontbase":
-				pd = new PlayerData(new FrontBase(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "firebird":
-				pd = new PlayerData(new Firebird(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "mongo":
-				pd = new PlayerData(new Mongo(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "msql":
-				pd = new PlayerData(new mSQL(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-			case "maxdb":
-				pd = new PlayerData(new MaxDB(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
-						"sql.port"), getConfig().getString("sql.database"), getConfig().getString("sql.username"), getConfig().getString("sql.password")));
-				break;
-		}
+		initDatabase();
 		
 		int et = (int) (System.currentTimeMillis() - st);
 		getLogger().info(name + " v" + version + " by " + author + " enabled in " + et + "ms.");
@@ -115,5 +49,76 @@ public class Maussentials extends JavaPlugin {
 		
 		int et = (int) (System.currentTimeMillis() - st);
 		getLogger().info(name + " v" + version + " by " + author + " disabled in " + et + "ms.");
+	}
+	
+	public void initDatabase() {
+		String database = getConfig().getString("sql.database");
+		switch (this.getConfig().getString("sql.type").toLowerCase(Locale.ENGLISH)) {
+			case "mysql":
+				md = new MauData(new MySQL(Logger.getLogger("Minecraft"), "Maussentials", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "sqlite":
+				md = new MauData(new SQLite(Logger.getLogger("Minecraft"), "Maussentials", this.getDataFolder().getPath(), database));
+				break;
+			case "h2":
+				md = new MauData(new H2(Logger.getLogger("Minecraft"), "Maussentials", this.getDataFolder().getPath(), database));
+				break;
+			case "postgresql":
+				md = new MauData(new PostgreSQL(Logger.getLogger("Minecraft"), "Maussentials", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "microsoftsql":
+			case "mssql":
+				try {
+					md = new MauData(new MicrosoftSQL(Logger.getLogger("Minecraft"), "Maussentials", getConfig().getString("sql.hostname"), getConfig()
+							.getInt("sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				} catch (SQLException e) {
+					getLogger().severe("Failed to initialize MicrosoftSQL connection");
+					e.printStackTrace();
+				}
+				break;
+			case "oracle":
+				try {
+					md = new MauData(new Oracle(Logger.getLogger("Minecraft"), "Maussentials", getConfig().getString("sql.hostname"), getConfig().getInt(
+							"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				} catch (SQLException e) {
+					getLogger().severe("Failed to initialize Oracle database connection");
+					e.printStackTrace();
+				}
+				break;
+			case "ingres":
+				md = new MauData(new Ingres(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "informix":
+				md = new MauData(new Informix(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "db2":
+				md = new MauData(new DB2(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "frontbase":
+				md = new MauData(new FrontBase(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "firebird":
+				md = new MauData(new Firebird(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "mongo":
+				md = new MauData(new Mongo(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "msql":
+				md = new MauData(new mSQL(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+			case "maxdb":
+				md = new MauData(new MaxDB(Logger.getLogger("Minecraft"), "[Maussentials] ", getConfig().getString("sql.hostname"), getConfig().getInt(
+						"sql.port"), database, getConfig().getString("sql.username"), getConfig().getString("sql.password")));
+				break;
+		}
 	}
 }
