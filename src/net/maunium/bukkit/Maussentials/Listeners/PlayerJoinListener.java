0413f7d5013dd5df32f8c8beadf2943c11ba4703
Tulir293 <tulir293@maunium.net>
2015-02-22 16:34:11 +0200

    Implemented MauModule and added welcome message functionality

--- src/net/maunium/bukkit/Maussentials/Listeners/PlayerJoinListener.java
+++ src/net/maunium/bukkit/Maussentials/Listeners/PlayerJoinListener.java
@@ -1,21 +1,37 @@
 package net.maunium.bukkit.Maussentials.Listeners;
 
+import java.util.List;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import net.maunium.bukkit.Maussentials.ChatFormatter;
+import net.maunium.bukkit.Maussentials.MauModule;
 import net.maunium.bukkit.Maussentials.Maussentials;
 
-public class PlayerJoinListener implements Listener {
+public class PlayerJoinListener implements Listener, MauModule {
 	private Maussentials plugin;
+	private String[] welcome;
 	
-	
-	public PlayerJoinListener(Maussentials plugin) {
+	@Override
+	public void initialize(Maussentials plugin) {
 		this.plugin = plugin;
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		reload();
+	}
+	
+	@Override
+	public void reload() {
+		List<String> s = plugin.getConfig().getStringList("welcome-message");
+		welcome = new String[s.size()];
+		int i = 0;
+		for (String ss : s)
+			welcome[i] = ChatFormatter.translateAll(ss);
 	}
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent evt) {
-		
+		evt.getPlayer().sendMessage(welcome);
 	}
 }
