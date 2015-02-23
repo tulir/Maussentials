24a3a3655600acccc6cba890111d53b2c99db39d
Tulir293 <tulir293@maunium.net>
2015-02-23 18:33:30 +0200

    Added basic i18n instance and a permission checking method for command executors.

--- src/net/maunium/bukkit/Maussentials/Maussentials.java
+++ src/net/maunium/bukkit/Maussentials/Maussentials.java
@@ -1,19 +1,24 @@
 package net.maunium.bukkit.Maussentials;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import lib.PatPeter.SQLibrary.Database;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.maunium.bukkit.Maussentials.Modules.DatabaseHandler;
 import net.maunium.bukkit.Maussentials.Modules.PlayerData;
 import net.maunium.bukkit.Maussentials.Modules.WelcomeMessage;
-import net.maunium.bukkit.Maussentials.Utils.MauModule;
+import net.maunium.bukkit.Maussentials.Modules.Util.MauModule;
+import net.maunium.bukkit.Maussentials.Utils.I18n;
+import net.maunium.bukkit.Maussentials.Utils.I18n.I15r;
 
-public class Maussentials extends JavaPlugin {
+public class Maussentials extends JavaPlugin implements I15r {
 	
 	public String version;
 	public final String name = "Maussentials", author = "Tulir293", stag = ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + name + ChatColor.DARK_GREEN + "] "
@@ -21,12 +26,19 @@ public class Maussentials extends JavaPlugin {
 	private Map<String, MauModule> modules = new HashMap<String, MauModule>();
 	private DatabaseHandler dbh;
 	private PlayerData pd;
+	private I18n i18n;
 	
 	@Override
 	public void onEnable() {
 		long st = System.currentTimeMillis();
 		version = this.getDescription().getVersion();
 		this.saveDefaultConfig();
+		
+		try {
+			this.i18n = I18n.createInstance(new File(this.getDataFolder(), "languages"), getConfig().getString("language"));
+		} catch (IOException e) {
+			die("Failed to initialize internationalization", e);
+		}
 		
 		enableModule("database", dbh = new DatabaseHandler());
 		enableModule("welcome-message", new WelcomeMessage());
@@ -46,20 +58,39 @@ public class Maussentials extends JavaPlugin {
 		getLogger().info(name + " v" + version + " by " + author + " disabled in " + et + "ms.");
 	}
 	
+	public void die(String message, Throwable error) {
+		getLogger().severe(message + ":");
+		error.printStackTrace();
+		getLogger().severe("Maussentials can not continue to function before the error is fixed.");
+		this.getPluginLoader().disablePlugin(this);
+	}
+	
 	public Database getDB() {
 		return dbh.getDB();
 	}
 	
-	public void reloadModule(String name){
+	public boolean checkPerms(CommandSender p, String permission) {
+		if(!p.hasPermission(permission)) {
+			p.sendMessage(translate("permission-error"));
+			return false;
+		} else return true;
+	}
+	
+	public void reloadModule(String name) {
 		getModule(name).reload();
 	}
 	
-	public MauModule getModule(String name){
+	public MauModule getModule(String name) {
 		return modules.get(name);
 	}
 	
 	public void enableModule(String name, MauModule m) {
 		m.initialize(this);
 		modules.put(name, m);
+	}
+	
+	@Override
+	public String translate(String node, Object... replace) {
+		return i18n.translate(node, replace);
 	}
 }
