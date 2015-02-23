35fed46588ccade5fb81a21f8714ec07ee8c56af
Tulir293 <tulir293@maunium.net>
2015-02-23 18:09:32 +0200

    Added CommandModule for modules that mainly contain a command executor.

---
+++ src/net/maunium/bukkit/Maussentials/Modules/Util/CommandModule.java
@@ -0,0 +1,11 @@
+package net.maunium.bukkit.Maussentials.Modules.Util;
+
+import org.bukkit.command.CommandExecutor;
+
+/**
+ * A module mainly used for a command executor.
+ * 
+ * @author Tulir293
+ * @since 0.1
+ */
+public interface CommandModule extends MauModule, CommandExecutor {}
