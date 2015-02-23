99a8947c7eed096fd29c932bbb6dfe377537abcf
Tulir293 <tulir293@maunium.net>
2015-02-23 18:09:57 +0200

    Added event module for modules mainly used for event handlers.

---
+++ src/net/maunium/bukkit/Maussentials/Modules/Util/EventModule.java
@@ -0,0 +1,11 @@
+package net.maunium.bukkit.Maussentials.Modules.Util;
+
+import org.bukkit.event.Listener;
+
+/**
+ * A module mainly used for an event listener.
+ * 
+ * @author Tulir293
+ * @since 0.1
+ */
+public interface EventModule extends MauModule, Listener {}
