2c37b714ee794cc829cb86b12f64e70541bdb76c
Tulir293 <tulir293@maunium.net>
2015-02-26 15:51:11 +0200

    Added experimental implementation for /kill

--- src/net/maunium/bukkit/Maussentials/Modules/Commands/CommandKill.java
+++ src/net/maunium/bukkit/Maussentials/Modules/Commands/CommandKill.java
@@ -2,36 +2,43 @@ package net.maunium.bukkit.Maussentials.Modules.Commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import net.maunium.bukkit.Maussentials.Maussentials;
 import net.maunium.bukkit.Maussentials.Modules.Util.CommandModule;
+import net.maunium.bukkit.Maussentials.Utils.PlayerUtils;
 
 public class CommandKill extends CommandModule {
 	private Maussentials plugin;
 	
 	@Override
 	public boolean execute(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length > 0){
-			// TODO: Kill implementation
+		if (args.length > 0) {
+			Player p = PlayerUtils.getPlayer(args[0]);
+			if (p != null) {
+				p.setHealth(0);
+				sender.sendMessage(plugin.stag + plugin.translate("kill.ed", p.getName()));
+			} else sender.sendMessage(plugin.errtag + plugin.translate("kill.notfound"));
+			return true;
 		}
 		return false;
 	}
-
+	
 	@Override
 	public void reload() {
 		plugin.getCommand("maukill").setExecutor(this);
 	}
-
+	
 	@Override
 	public void initialize(Maussentials plugin) {
 		this.plugin = plugin;
 		this.permission = "maussentials.kill";
 		reload();
 	}
-
+	
 	@Override
 	public void help(CommandSender sender, Command command, String label, String[] args) {
-		// TODO: Help for /kill
+		sender.sendMessage(plugin.stag + plugin.translate("kill.help", label));
 	}
 	
 }
