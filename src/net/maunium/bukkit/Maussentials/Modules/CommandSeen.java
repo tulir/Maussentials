6076488eb2544fb9c925a314882f0ac54ebff57e
Tulir293 <tulir293@maunium.net>
2015-02-28 13:33:31 +0200

    Added CommandSeen but not yet implemented the command itself

---
+++ src/net/maunium/bukkit/Maussentials/Modules/CommandSeen.java
@@ -0,0 +1,34 @@
+package net.maunium.bukkit.Maussentials.Modules;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import net.maunium.bukkit.Maussentials.Maussentials;
+import net.maunium.bukkit.Maussentials.Modules.Util.CommandModule;
+
+public class CommandSeen extends CommandModule {
+	private Maussentials plugin;
+	
+	@Override
+	public void initialize(Maussentials plugin) {
+		this.plugin = plugin;
+		reload();
+	}
+	
+	@Override
+	public void reload() {
+		plugin.getCommand("mauseen").setExecutor(this);
+	}
+	
+	@Override
+	public boolean execute(CommandSender sender, Command cmd, String label, String[] args) {
+		// TODO: Seen command implementation
+		return false;
+	}
+	
+	@Override
+	public void help(CommandSender sender, Command cmd, String label, String[] args) {
+		sender.sendMessage(plugin.stag + plugin.translate("seen.help"));
+	}
+	
+}
