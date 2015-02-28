c249641eac4cd8c6b3f60be075ddebbffd5c9d78
Tulir293 <tulir293@maunium.net>
2015-02-28 17:27:18 +0200

    Actually fixed messages starting with a color code

--- src/net/maunium/bukkit/Maussentials/Utils/ChatFormatter.java
+++ src/net/maunium/bukkit/Maussentials/Utils/ChatFormatter.java
@@ -21,7 +21,7 @@ public class ChatFormatter {
 	public static String translate(char alt, String toFormat, String colorChars) {
 		char[] b = toFormat.toCharArray();
 		for (int i = 0; i < b.length - 1; i++) {
-			if (b[i] == alt && (i - 1 >= 0 && b[i - 1] != alt) && colorChars.indexOf(b[i + 1]) > -1) {
+			if (b[i] == alt && (i - 1 >= 0 || b[i - 1] != alt) && colorChars.indexOf(b[i + 1]) > -1) {
 				b[i] = ChatColor.COLOR_CHAR;
 				b[i + 1] = Character.toLowerCase(b[i + 1]);
 			}
