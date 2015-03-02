026a81400272f827906b6d27daabe0afec575337
Tulir293 <tulir293@maunium.net>
2015-03-02 08:27:31 +0200

    Disabled console things after noticing that it's a player-only command <.<

--- src/net/maunium/bukkit/Maussentials/Modules/Godmode.java
+++ src/net/maunium/bukkit/Maussentials/Modules/Godmode.java
@@ -2,7 +2,6 @@ package net.maunium.bukkit.Maussentials.Modules;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -53,7 +52,7 @@ public class Godmode extends PlayerCommandModule implements Listener {
 		if (args.length == 0) {
 			sender.sendMessage(plugin.stag + plugin.translate("god." + (toggle_def(sender) ? "off" : "on.def")));
 		} else if (args.length > 0) {
-			if (args[0].equalsIgnoreCase("default")) {
+			if (args[0].equalsIgnoreCase("default") || args[0].equalsIgnoreCase("def")) {
 				if (args.length > 1 && args[1].equalsIgnoreCase("private")) MetadataUtils.setFixedMetadata(sender, PRIVATE_GOD, true, plugin);
 				sender.sendMessage(plugin.stag + plugin.translate("god." + (toggle(DEFAULT_GOD, sender) ? "on.def" : "off")));
 			} else if (args[0].equalsIgnoreCase("damage") || args[0].equalsIgnoreCase("dmg")) {
@@ -63,21 +62,18 @@ public class Godmode extends PlayerCommandModule implements Listener {
 				Player p = plugin.getServer().getPlayer(args[0]);
 				if (p != null) {
 					if (args.length == 1) {
-						if (p.hasMetadata(PRIVATE_GOD) && !(sender instanceof ConsoleCommandSender))
-							sender.sendMessage(plugin.errtag + plugin.translate("god.private", p.getName()));
-						sender.sendMessage(plugin.stag + plugin.translate("god." + (toggle_def(p) ? "off" : "on.def") + ".for", p.getName()));
+						if (p.hasMetadata(PRIVATE_GOD)) sender.sendMessage(plugin.errtag + plugin.translate("god.private", p.getName()));
+						else sender.sendMessage(plugin.stag + plugin.translate("god.for." + (toggle_def(p) ? "off" : "on.def"), p.getName()));
 					} else {
 						if (args[1].equalsIgnoreCase("default")) {
-							if (p.hasMetadata(PRIVATE_GOD) && !(sender instanceof ConsoleCommandSender)) sender.sendMessage(plugin.errtag
-									+ plugin.translate("god.private", p.getName()));
-							else sender.sendMessage(plugin.stag + plugin.translate("god." + (toggle(DEFAULT_GOD, p) ? "on.def" : "off") + ".for", p.getName()));
+							if (p.hasMetadata(PRIVATE_GOD)) sender.sendMessage(plugin.errtag + plugin.translate("god.private", p.getName()));
+							else sender.sendMessage(plugin.stag + plugin.translate("god.for." + (toggle(DEFAULT_GOD, p) ? "on.def" : "off"), p.getName()));
 						} else if (args[1].equalsIgnoreCase("damage")) {
-							if (p.hasMetadata(PRIVATE_GOD) && !(sender instanceof ConsoleCommandSender)) sender.sendMessage(plugin.errtag
-									+ plugin.translate("god.private", p.getName()));
-							else sender.sendMessage(plugin.stag + plugin.translate("god." + (toggle(DAMAGE_GOD, p) ? "on.dmg" : "off") + ".for", p.getName()));
+							if (p.hasMetadata(PRIVATE_GOD)) sender.sendMessage(plugin.errtag + plugin.translate("god.private", p.getName()));
+							else sender.sendMessage(plugin.stag + plugin.translate("god.for." + (toggle(DAMAGE_GOD, p) ? "on.dmg" : "off"), p.getName()));
 						}
 					}
-				} else sender.sendMessage(plugin.errtag + plugin.translate("god.notfound", label));
+				} else sender.sendMessage(plugin.errtag + plugin.translate("god.notfound", args[0]));
 			}
 		}
 		return true;
