cf75b47a9cb0ee7585f3fffef9661d87927bed51
Tulir293 <tulir293@maunium.net>
2015-03-07 14:37:51 +0200

    Separated sign coloring into own listener

--- src/net/maunium/bukkit/Maussentials/Modules/SignEditor.java
+++ src/net/maunium/bukkit/Maussentials/Modules/SignEditor.java
@@ -10,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.metadata.MetadataValue;
 
@@ -53,26 +54,34 @@ public class SignEditor extends CommandModule implements Listener {
 		if ((evt.getAction().equals(Action.RIGHT_CLICK_BLOCK) || evt.getAction().equals(Action.LEFT_CLICK_BLOCK))
 				&& evt.getClickedBlock().getType().equals(Material.SIGN) && evt.getPlayer().hasMetadata(EDIT_META)) {
 			MetadataValue mv = MetadataUtils.getMetadata(evt.getPlayer(), EDIT_META, plugin);
-			String[] lines = new String[4];
-			boolean color = evt.getPlayer().hasPermission("maussentials.sign.format.color");
-			boolean style = evt.getPlayer().hasPermission("maussentials.sign.format.style");
-			boolean magic = evt.getPlayer().hasPermission("maussentials.sign.format.magic");
 			int i = 0;
 			Sign x = (Sign) evt.getClickedBlock().getState();
 			for (Object s : (List<?>) mv.value()) {
 				if (i > 3) break;
-				// Use a more efficient technique to translate colors if all of the color permissions apply.
-				if (color && style && magic) x.setLine(i, ChatFormatter.formatAll(s.toString()));
-				// Format each separately if not all of the permissions apply.
-				else {
-					String ss = s.toString();
-					if (color) ss = ChatFormatter.formatColors(ss);
-					if (style) ss = ChatFormatter.formatColors(ss);
-					if (magic) ss = ChatFormatter.formatColors(ss);
-					x.setLine(i, ss);
-				}
+				x.setLine(i, s.toString());
 				i++;
 			}
+		}
+	}
+	
+	@EventHandler
+	public void onSignFinish(SignChangeEvent evt) {
+		boolean color = evt.getPlayer().hasPermission("maussentials.sign.format.color");
+		boolean style = evt.getPlayer().hasPermission("maussentials.sign.format.style");
+		boolean magic = evt.getPlayer().hasPermission("maussentials.sign.format.magic");
+		
+		if (!color && !style && !magic) return;
+		
+		int i = 0;
+		for (String s : evt.getLines()) {
+			if (color && style && magic) evt.setLine(i, ChatFormatter.formatAll(s));
+			else {
+				if (color) s = ChatFormatter.formatColors(s);
+				if (style) s = ChatFormatter.formatStyles(s);
+				if (magic) s = ChatFormatter.formatMagic(s);
+				evt.setLine(i, s);
+			}
+			i++;
 		}
 	}
 }
