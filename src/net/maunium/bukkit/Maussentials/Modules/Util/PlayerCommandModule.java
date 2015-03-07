55b004486a3ef611651f4935b624d347597b9948
Tulir293 <tulir293@maunium.net>
2015-03-07 20:57:57 +0200

    Now has help method with player argument instead of CommandSender. Also added javadoc

--- src/net/maunium/bukkit/Maussentials/Modules/Util/PlayerCommandModule.java
+++ src/net/maunium/bukkit/Maussentials/Modules/Util/PlayerCommandModule.java
@@ -6,15 +6,44 @@ import org.bukkit.entity.Player;
 
 import net.maunium.bukkit.Maussentials.Maussentials;
 
+/**
+ * A module mainly used for a command executor of a in-game-only command.
+ * 
+ * @author Tulir293
+ * @since 0.1
+ */
 public abstract class PlayerCommandModule extends CommandModule {
 	@Override
 	public final boolean execute(CommandSender sender, Command cmd, String label, String[] args) {
-		if(!(sender instanceof Player)){
-			sender.sendMessage(Maussentials.getInstance().errtag + Maussentials.getInstance().translate("must-be-ingame"));
-			return true;
-		}
-		return execute((Player) sender, cmd, label, args);
+		if (!(sender instanceof Player)) sender.sendMessage(Maussentials.getInstance().errtag + Maussentials.getInstance().translate("must-be-ingame"));
+		else return execute((Player) sender, cmd, label, args);
+		return true;
 	}
 	
+	@Override
+	public final void help(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player)) sender.sendMessage(Maussentials.getInstance().errtag + Maussentials.getInstance().translate("must-be-ingame"));
+		else help((Player) sender, cmd, label, args);
+	}
+	
+	/**
+	 * Execute the command.
+	 * 
+	 * @param sender The Player who is executing the command.
+	 * @param cmd The command object. The getName() method in this returns the main command name.
+	 * @param label The label used. May not be the main command name.
+	 * @param args The arguments.
+	 * @return If the syntax of the command was correct or not. If false, will print a help message.
+	 */
 	public abstract boolean execute(Player sender, Command cmd, String label, String[] args);
+	
+	/**
+	 * Print the help message.
+	 * 
+	 * @param sender The Player who tried to execute a command with incorrect syntax.
+	 * @param cmd The command object.
+	 * @param label The label used. Useful if you want to show an usage example with the same label as the sender used.
+	 * @param args The arguments used.
+	 */
+	public abstract void help(Player sender, Command cmd, String label, String[] args);
 }
