cec9bcac72ae29366cfbc55bfd04f0c6c774655c
Tulir293 <tulir293@maunium.net>
2015-03-07 21:12:50 +0200

    Added Javadoc. Changed AreaFormatException constructor to private and removed all of them except the one with String and Throwable as arguments. Changed all throwings of an AreaFormatException to include the throwable that caused the exception.

--- src/net/maunium/bukkit/Maussentials/Utils/Area.java
+++ src/net/maunium/bukkit/Maussentials/Utils/Area.java
@@ -18,12 +18,29 @@ import org.bukkit.configuration.serialization.SerializableAs;
 @SerializableAs("MauArea")
 public class Area implements Serializable, ConfigurationSerializable {
 	private static final long serialVersionUID = 6042751882876629285L;
+	public static final String MINMAX_SPLITTER = " | ", NUMSPLITTER = ", ";
 	private final int minX, minY, minZ, maxX, maxY, maxZ;
 	
-	public Area(Location c1, Location c2){
+	/**
+	 * Create an area with the coordinates of the given locations.
+	 * 
+	 * @param c1 The first corner.
+	 * @param c2 The second corner.
+	 */
+	public Area(Location c1, Location c2) {
 		this(c1.getBlockX(), c1.getBlockY(), c1.getBlockZ(), c2.getBlockX(), c2.getBlockY(), c2.getBlockZ());
 	}
 	
+	/**
+	 * Creates an area with the given coordinates.
+	 * 
+	 * @param x1 The X coordinate of the first corner.
+	 * @param y1 The Y coordinate of the first corner.
+	 * @param z1 The Z coordinate of the first corner.
+	 * @param x2 The X coordinate of the second corner.
+	 * @param y2 The Y coordinate of the second corner.
+	 * @param z2 The Z coordinate of the second corner.
+	 */
 	public Area(int x1, int y1, int z1, int x2, int y2, int z2) {
 		if (x1 < x2 || x1 == x2) {
 			minX = x1;
@@ -50,6 +67,13 @@ public class Area implements Serializable, ConfigurationSerializable {
 		}
 	}
 	
+	/**
+	 * Creates an area using the given serialized map. This should not be used directly, as it is intended for the
+	 * Bukkit YAML configuration serialization system.
+	 * 
+	 * @param serialized The serialized map.
+	 * @throws AreaFormatException If deserialization fails.
+	 */
 	public Area(Map<String, Object> serialized) {
 		try {
 			minX = Integer.parseInt(serialized.get("min-x").toString());
@@ -59,40 +83,45 @@ public class Area implements Serializable, ConfigurationSerializable {
 			maxY = Integer.parseInt(serialized.get("max-y").toString());
 			maxZ = Integer.parseInt(serialized.get("max-z").toString());
 		} catch (NumberFormatException | NullPointerException e) {
-			throw new AreaFormatException("Couldn't deserialize given map.");
+			throw new AreaFormatException("Couldn't deserialize given map.", e);
 		}
 	}
 	
-	public static final String MINMAX_SPLITTER = " | ", NUMSPLITTER = ", ";
-	
+	/**
+	 * A textual representation of this Area in the following format:<br>
+	 * <code>minX, minY, minZ | maxX, maxY, maxZ</code>
+	 */
 	@Override
 	public String toString() {
 		return minX + NUMSPLITTER + minY + NUMSPLITTER + minZ + MINMAX_SPLITTER + maxX + NUMSPLITTER + maxY + NUMSPLITTER + maxZ;
 	}
 	
-	public static String toString(Area a) {
-		return a.toString();
-	}
-	
+	/**
+	 * Parses an Area from a String.
+	 * 
+	 * @param s The String to parse an Area from.
+	 * @return The parsed Area.
+	 * @throws AreaFormatException If parsing fails
+	 */
 	public static Area fromString(String s) {
 		String[] ss;
 		try {
 			ss = s.split(MINMAX_SPLITTER, 2);
 		} catch (ArrayIndexOutOfBoundsException e) {
-			throw new AreaFormatException("String doesn't contain the Min-Max splitter char");
+			throw new AreaFormatException("String doesn't contain the Min-Max splitter char", e);
 		}
 		
 		String[] min, max;
 		try {
 			min = ss[0].split(NUMSPLITTER, 3);
 		} catch (ArrayIndexOutOfBoundsException e) {
-			throw new AreaFormatException("Min string doesn't contain the Number splitter char");
+			throw new AreaFormatException("Min string doesn't contain the Number splitter char", e);
 		}
 		
 		try {
 			max = ss[1].split(NUMSPLITTER, 3);
 		} catch (ArrayIndexOutOfBoundsException e) {
-			throw new AreaFormatException("Min string doesn't contain the Number splitter char");
+			throw new AreaFormatException("Min string doesn't contain the Number splitter char", e);
 		}
 		
 		int minX, minY, minZ, maxX, maxY, maxZ;
@@ -112,66 +141,105 @@ public class Area implements Serializable, ConfigurationSerializable {
 			parsing = "Max Z";
 			maxZ = Integer.parseInt(max[2]);
 		} catch (NumberFormatException e) {
-			throw new AreaFormatException("Number format exception while parsing " + parsing);
+			throw new AreaFormatException("Number format exception while parsing " + parsing, e);
 		}
 		
 		return new Area(minX, minY, minZ, maxX, maxY, maxZ);
 	}
 	
+	/**
+	 * Get the corner with the smaller values as a Location in the given world.
+	 * 
+	 * @param w The world to use for the location.
+	 * @return The location.
+	 */
 	public Location getMinCorner(World w) {
 		return new Location(w, minX, minY, minZ);
 	}
 	
+	/**
+	 * Get the corner with the larger values as a Location in the given world.
+	 * 
+	 * @param w The world to use for the location.
+	 * @return The location.
+	 */
 	public Location getMaxCorner(World w) {
 		return new Location(w, maxX, maxY, maxZ);
 	}
 	
+	/**
+	 * Check if a location is in this Area.
+	 * 
+	 * @param l The location to check.
+	 * @return True if the given location is in this Area, false otherwise.
+	 */
 	public boolean isInArea(Location l) {
 		return isInArea(l.getBlockX(), l.getBlockY(), l.getBlockZ());
 	}
 	
+	/**
+	 * Check if the given coordinates are in this Area.
+	 * 
+	 * @param x The X coordinate.
+	 * @param y The Y coordinate.
+	 * @param z The Z coordinate.
+	 * @return True if the given coordinates are in this Area, false otherwise.
+	 */
 	public boolean isInArea(int x, int y, int z) {
 		return x >= minX && x <= maxX && y >= minY && y <= maxY && z >= minZ && z <= maxZ;
 	}
 	
+	/**
+	 * @return The smaller X value of this Area.
+	 */
 	public int getMinX() {
 		return minX;
 	}
 	
+	/**
+	 * @return The smaller Y value of this Area.
+	 */
 	public int getMinY() {
 		return minY;
 	}
 	
+	/**
+	 * @return The smaller Z value of this Area.
+	 */
 	public int getMinZ() {
 		return minZ;
 	}
 	
+	/**
+	 * @return The larger X value of this Area.
+	 */
 	public int getMaxX() {
 		return maxX;
 	}
 	
+	/**
+	 * @return The larger Y value of this Area.
+	 */
 	public int getMaxY() {
 		return maxY;
 	}
 	
+	/**
+	 * @return The larger Z value of this Area.
+	 */
 	public int getMaxZ() {
 		return maxZ;
 	}
 	
+	/**
+	 * An exception that is thrown when failing to parse or deserialize an Area.
+	 * 
+	 * @author Tulir293
+	 * @since 0.1
+	 */
 	public static class AreaFormatException extends IllegalArgumentException {
 		private static final long serialVersionUID = 4080468767898580267L;
-		
-		public AreaFormatException() {}
-		
-		public AreaFormatException(String msg) {
-			super(msg);
-		}
-		
-		public AreaFormatException(Throwable cause) {
-			super(cause);
-		}
-		
-		public AreaFormatException(String msg, Throwable cause) {
+		private AreaFormatException(String msg, Throwable cause) {
 			super(msg, cause);
 		}
 	}
