e7f09fdb7d4dbd3b8752acefcc334084ecd5b50a
Tulir293 <tulir293@maunium.net>
2015-03-08 11:43:31 +0200

    Added JoinListener for MauBans and added IP-ban checking inside it.

---
+++ src/net/maunium/bukkit/Maussentials/Modules/Bans/JoinListener.java
@@ -0,0 +1,51 @@
+package net.maunium.bukkit.Maussentials.Modules.Bans;
+
+import java.sql.ResultSet;
+import java.util.UUID;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
+
+import net.maunium.bukkit.Maussentials.Maussentials;
+import net.maunium.bukkit.Maussentials.Utils.DateUtils;
+
+public class JoinListener implements Listener {
+	private Maussentials plugin;
+	private MauBans host;
+	
+	public JoinListener(Maussentials plugin, MauBans host) {
+		this.plugin = plugin;
+		this.host = host;
+	}
+	
+	@EventHandler
+	public void onPreJoin(AsyncPlayerPreLoginEvent evt) {
+		try {
+			ResultSet rs = host.getBanData(evt.getAddress().getHostAddress(), MauBans.TYPE_IP);
+			if (rs != null) {
+				String reason = rs.getString(MauBans.COLUMN_REASON);
+				String bannedBy = rs.getString(MauBans.COLUMN_BANNEDBY);
+				long expireExact = rs.getLong(MauBans.COLUMN_EXPIRE);
+				
+				if (!bannedBy.equals("CONSOLE")) {
+					UUID u = UUID.fromString(bannedBy);
+					bannedBy = plugin.getServer().getOfflinePlayer(u).getName();
+				}
+				
+				if (expireExact > 0) {
+					String expire = DateUtils.getDurationBreakdown(expireExact - System.currentTimeMillis(), DateUtils.MODE_IN);
+					evt.setLoginResult(Result.KICK_BANNED);
+					evt.setKickMessage(plugin.translatePlain("bans.ipban.temporary", reason, bannedBy, expire, evt.getAddress().getHostAddress()));
+				} else {
+					evt.setLoginResult(Result.KICK_BANNED);
+					evt.setKickMessage(plugin.translatePlain("bans.ipban.permanent", reason, bannedBy, evt.getAddress().getHostAddress()));
+				}
+			}
+		} catch (Exception e) {
+			plugin.getLogger().severe("Failed to check if " + evt.getAddress().getHostAddress() + " is banned: ");
+			e.printStackTrace();
+		}
+	}
+}
