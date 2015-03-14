045e71776f34cee080b4254c400297e869943e09
Tulir293 <tulir293@maunium.net>
2015-03-14 13:29:10 +0200

    More logic

--- src/net/maunium/bukkit/Maussentials/Modules/Teleportation/CommandTP.java
+++ src/net/maunium/bukkit/Maussentials/Modules/Teleportation/CommandTP.java
@@ -21,43 +21,127 @@ public class CommandTP implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length > 0) {
 			boolean exact = false, force = false, safe = false;
-			Player p = null;
+			int nextArg = 0;
+			Player p1 = null, p2 = null, relTo = null;
 			double x = Double.NaN, y = Double.NaN, z = Double.NaN;
 			float yaw = Float.NaN, pitch = Float.NaN;
 			World w = null;
 			
 			for (String s : args) {
-				if (s.startsWith("-")) {
+				if (nextArg == 1) {
+					Player p = gplayer(sender, s);
+					if (p != null) relTo = p;
+					nextArg = 0;
+				} else if (nextArg == 2) {
+					Player p = gplayer(sender, s);
+					if (p != null) p1 = p;
+					nextArg = 0;
+				} else if (nextArg == 3) {
+					Player p = gplayer(sender, s);
+					if (p != null) p2 = p;
+					nextArg = 0;
+				} else if (nextArg == 10) {
+					double d = gdouble(sender, s);
+					if (d != Double.NaN) z = d;
+					nextArg = 0;
+				} else if (nextArg == 11) {
+					double d = gdouble(sender, s);
+					if (d != Double.NaN) y = d;
+					nextArg = 0;
+				} else if (nextArg == 12) {
+					double d = gdouble(sender, s);
+					if (d != Double.NaN) z = d;
+					nextArg = 0;
+				} else if (nextArg == 13) {
+					float f = gfloat(sender, s);
+					if (f != Float.NaN) yaw = f;
+					nextArg = 0;
+				} else if (nextArg == 14) {
+					float f = gfloat(sender, s);
+					if (f != Float.NaN) pitch = f;
+					nextArg = 0;
+				} else if (nextArg == 15) {
+					World ww = gworld(sender, s);
+					if (ww != null) w = ww;
+					nextArg = 0;
+				} else if (s.startsWith("-")) {
 					s = s.substring(1);
 					if (s.startsWith("-")) {
 						s = s.substring(1);
-						if (s.equalsIgnoreCase("exact")) {
-							if (sender.hasPermission("maussentials.tp.exact")) exact = true;
-							else sender.sendMessage(plugin.translateErr("tp.no-perms", "exact"));
-						} else if (s.equalsIgnoreCase("force")) {
-							if (sender.hasPermission("maussentials.tp.force")) force = true;
-							else sender.sendMessage(plugin.translateErr("tp.no-perms", "force"));
-						} else if (s.equalsIgnoreCase("safe")) {
-							if (sender.hasPermission("maussentials.tp.safe")) safe = true;
-							else sender.sendMessage(plugin.translateErr("tp.no-perms", "safe"));
-						}
+						if (s.equalsIgnoreCase("exact")) exact = true;
+						else if (s.equalsIgnoreCase("force")) force = true;
+						else if (s.equalsIgnoreCase("safe")) safe = true;
+						else if (s.equalsIgnoreCase("relative")) nextArg = 1;
+						else if (s.equalsIgnoreCase("player1")) nextArg = 2;
+						else if (s.equalsIgnoreCase("player2")) nextArg = 3;
+						else if (s.equalsIgnoreCase("xpos")) nextArg = 10;
+						else if (s.equalsIgnoreCase("ypos")) nextArg = 11;
+						else if (s.equalsIgnoreCase("zpos")) nextArg = 12;
+						else if (s.equalsIgnoreCase("yaw")) nextArg = 13;
+						else if (s.equalsIgnoreCase("pitch")) nextArg = 14;
+						else if (s.equalsIgnoreCase("world")) nextArg = 15;
 					} else {
-						if (s.equalsIgnoreCase("e")) {
-							if (sender.hasPermission("maussentials.tp.exact")) exact = true;
-							else sender.sendMessage(plugin.translateErr("tp.no-perms", "exact"));
-						} else if (s.equalsIgnoreCase("f")) {
-							if (sender.hasPermission("maussentials.tp.force")) force = true;
-							else sender.sendMessage(plugin.translateErr("tp.no-perms", "force"));
-						} else if (s.equalsIgnoreCase("s")) {
-							if (sender.hasPermission("maussentials.tp.safe")) safe = true;
-							else sender.sendMessage(plugin.translateErr("tp.no-perms", "safe"));
-						}
+						if (s.equalsIgnoreCase("e")) exact = true;
+						else if (s.equalsIgnoreCase("f")) force = true;
+						else if (s.equalsIgnoreCase("s")) safe = true;
+						else if (s.equalsIgnoreCase("r")) nextArg = 1;
+						else if (s.equalsIgnoreCase("p1")) nextArg = 2;
+						else if (s.equalsIgnoreCase("p2")) nextArg = 3;
+						else if (s.equalsIgnoreCase("x")) nextArg = 10;
+						else if (s.equalsIgnoreCase("y")) nextArg = 11;
+						else if (s.equalsIgnoreCase("z")) nextArg = 12;
+						else if (s.equalsIgnoreCase("w")) nextArg = 15;
 					}
 				} else {
-					
+					Player p = plugin.getServer().getPlayer(s);
+					if (p != null) {
+						if (p1 == null) p1 = p;
+						else if (p2 == null) p2 = p;
+						else plugin.translateErr("tp.args.too-many-players");
+					} else {
+						World ww = plugin.getServer().getWorld(s);
+						if (ww != null) {
+							if (w == null) w = ww;
+							else plugin.translateErr("tp.args.too-many-worlds");
+						} else {
+							// TODO: X, Y, Z, Yaw, Pitch
+						}
+					}
 				}
 			}
 		}
 		return false;
+	}
+	
+	private float gfloat(CommandSender sender, String s) {
+		try {
+			return Float.parseFloat(s);
+		} catch (NumberFormatException e) {
+			sender.sendMessage(plugin.translateErr("tp.args.nfe", s));
+			return Float.NaN;
+		}
+	}
+	
+	private double gdouble(CommandSender sender, String s) {
+		try {
+			return Double.parseDouble(s);
+		} catch (NumberFormatException e) {
+			sender.sendMessage(plugin.translateErr("tp.args.nfe", s));
+			return Double.NaN;
+		}
+	}
+	
+	private Player gplayer(CommandSender sender, String s) {
+		Player p = plugin.getServer().getPlayer(s);
+		if (p != null) return p;
+		else sender.sendMessage(plugin.translateErr("tp.args.pnf", s));
+		return null;
+	}
+	
+	private World gworld(CommandSender sender, String s) {
+		World p = plugin.getServer().getWorld(s);
+		if (p != null) return p;
+		else sender.sendMessage(plugin.translateErr("tp.args.wnf", s));
+		return null;
 	}
 }
