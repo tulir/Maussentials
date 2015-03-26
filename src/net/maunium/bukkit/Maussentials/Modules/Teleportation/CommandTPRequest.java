b380253f0382d0d95bf7327523145948388e0c23
Tulir293 <tulir293@maunium.net>
2015-03-26 15:22:08 +0200

    Added executor

--- src/net/maunium/bukkit/Maussentials/Modules/Teleportation/CommandTPRequest.java
+++ src/net/maunium/bukkit/Maussentials/Modules/Teleportation/CommandTPRequest.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 
 import net.maunium.bukkit.Maussentials.Maussentials;
 import net.maunium.bukkit.Maussentials.Utils.IngameMauCommandExecutor;
+import net.maunium.bukkit.Maussentials.Utils.MetadataUtils;
 
 public class CommandTPRequest implements IngameMauCommandExecutor {
 	private Maussentials plugin;
@@ -15,11 +16,20 @@ public class CommandTPRequest implements IngameMauCommandExecutor {
 	
 	@Override
 	public boolean execute(Player sender, Command cmd, String label, String[] args) {
+		if (args.length > 0) {
+			Player p = plugin.getServer().getPlayer(args[0]);
+			if (p != null) {
+				MetadataUtils.setFixedMetadata(p, MauTPs.TP_REQUEST_META, sender.getUniqueId(), plugin);
+				p.sendMessage(plugin.translateStd("tp.request.from", sender.getName()));
+				sender.sendMessage(plugin.translateStd("tp.request.to", p.getName()));
+			} else sender.sendMessage(plugin.translateErr("tp.request.playernotfound", args[0]));
+			return true;
+		}
 		return false;
 	}
 	
 	@Override
 	public void help(Player sender, Command cmd, String label, String[] args) {
-		
+		sender.sendMessage(plugin.translateErr("tp.request.help", label));
 	}
 }
