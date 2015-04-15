88b4db9a64e7b2548dea05f6ff1b8460e394de60
Tulir293 <tulir293@maunium.net>
2015-04-15 17:39:44 +0300

    Flip onFail and onSuccess arguments

--- src/net/maunium/bukkit/Maussentials/Utils/DelayedActions/DelayedAction.java
+++ src/net/maunium/bukkit/Maussentials/Utils/DelayedActions/DelayedAction.java
@@ -11,8 +11,8 @@ import net.maunium.bukkit.Maussentials.Maussentials;
 import net.maunium.bukkit.Maussentials.Utils.MetadataUtils;
 
 /**
- * A simple system for delayed actions. Also has option to allow faster (or slower) action running if nobody is within a
- * given range and you can even add a list of exceptions to who can be in that range.
+ * A simple system for delayed actions. Also has option to allow faster (or slower) action running if nobody is within a given range and you can even add a list
+ * of exceptions to who can be in that range.
  * 
  * @author Tulir293
  * @since 0.1
@@ -30,21 +30,19 @@ public class DelayedAction {
 	 * Creates an Delayed Action instance. All the non-primitive arguments except p can be null.
 	 * 
 	 * @param p The player to teleport.
-	 * @param delay The delay in ticks to use for teleportation by default. This may be overriden by safeDelay if
-	 *            certain conditions are met.
-	 * @param onFail The runnable to run after a canceled teleport.
+	 * @param delay The delay in ticks to use for teleportation by default. This may be overriden by safeDelay if certain conditions are met.
 	 * @param onSuccess The runnable to run after a successful teleport.
-	 * @param safeRange If there are no players in the range or all the players in the range are in the noCancel array,
-	 *            the player will use the safeDelay instead for delay for the delay of the teleportation. Setting to 0
-	 *            or less disables the safeRange system.
-	 * @param safeDelay The delay in ticks to use instead if all the players in the safeRange are in the noCancel list.
-	 *            Setting to the same as delay or -1 disables the safeRange system.
-	 * @param noCancel The list of players who can be near the player without disallowing use of safeDelay as delay. Can
-	 *            be null and will not affect the status of the safeRange system if null or empty.
+	 * @param onFail The runnable to run after a canceled teleport.
+	 * @param safeRange If there are no players in the range or all the players in the range are in the noCancel array, the player will use the safeDelay
+	 *            instead for delay for the delay of the teleportation. Setting to 0 or less disables the safeRange system.
+	 * @param safeDelay The delay in ticks to use instead if all the players in the safeRange are in the noCancel list. Setting to the same as delay or -1
+	 *            disables the safeRange system.
+	 * @param noCancel The list of players who can be near the player without disallowing use of safeDelay as delay. Can be null and will not affect the status
+	 *            of the safeRange system if null or empty.
 	 * @throws IllegalArgumentException if p is null.
 	 */
-	public DelayedAction(Player p, int delay, Runnable onFail, Runnable onSuccess, double safeRange, int safeDelay, List<UUID> noCancel) {
-		this.plugin = Maussentials.getInstance();
+	public DelayedAction(Player p, int delay, Runnable onSuccess, Runnable onFail, double safeRange, int safeDelay, List<UUID> noCancel) {
+		plugin = Maussentials.getInstance();
 		if (p == null) throw new IllegalArgumentException("Player can't be null!");
 		this.p = p;
 		this.delay = delay;
@@ -59,33 +57,30 @@ public class DelayedAction {
 	 * Creates an Delayed Action instance. All the non-primitive arguments except p can be null.
 	 * 
 	 * @param p The player to teleport.
-	 * @param delay The delay in ticks to use for teleportation by default. This may be overriden by safeDelay if
-	 *            certain conditions are met.
-	 * @param onFail The runnable to run after a canceled teleport.
+	 * @param delay The delay in ticks to use for teleportation by default. This may be overriden by safeDelay if certain conditions are met.
 	 * @param onSuccess The runnable to run after a successful teleport.
-	 * @param safeRange If there are no players in the range or all the players in the range are in the noCancel array,
-	 *            the player will use the safeDelay instead for delay for the delay of the teleportation. Setting to 0
-	 *            or less disables the safeRange system.
-	 * @param safeDelay The delay in ticks to use instead if all the players in the safeRange are in the noCancel list.
-	 *            Setting to the same as delay or -1 disables the safeRange system.
+	 * @param onFail The runnable to run after a canceled teleport.
+	 * @param safeRange If there are no players in the range or all the players in the range are in the noCancel array, the player will use the safeDelay
+	 *            instead for delay for the delay of the teleportation. Setting to 0 or less disables the safeRange system.
+	 * @param safeDelay The delay in ticks to use instead if all the players in the safeRange are in the noCancel list. Setting to the same as delay or -1
+	 *            disables the safeRange system.
 	 * @throws IllegalArgumentException if p is null.
 	 */
-	public DelayedAction(Player p, int delay, Runnable onFail, Runnable onSuccess, double safeRange, int safeDelay) {
-		this(p, delay, onFail, onSuccess, safeRange, safeDelay, null);
+	public DelayedAction(Player p, int delay, Runnable onSuccess, Runnable onFail, double safeRange, int safeDelay) {
+		this(p, delay, onSuccess, onFail, safeRange, safeDelay, null);
 	}
 	
 	/**
 	 * Creates an Delayed Action instance. All the non-primitive arguments except p can be null.
 	 * 
 	 * @param p The player to teleport.
-	 * @param delay The delay in ticks to use for teleportation by default. This may be overriden by safeDelay if
-	 *            certain conditions are met.
-	 * @param onFail The runnable to run after a canceled teleport.
+	 * @param delay The delay in ticks to use for teleportation by default. This may be overriden by safeDelay if certain conditions are met.
 	 * @param onSuccess The runnable to run after a successful teleport.
+	 * @param onFail The runnable to run after a canceled teleport.
 	 * @throws IllegalArgumentException if p is null.
 	 */
-	public DelayedAction(Player p, int delay, Runnable onFail, Runnable onSuccess) {
-		this(p, delay, onFail, onSuccess, -1, -1, null);
+	public DelayedAction(Player p, int delay, Runnable onSuccess, Runnable onFail) {
+		this(p, delay, onSuccess, onFail, -1, -1, null);
 	}
 	
 	public void failed() {
@@ -94,8 +89,8 @@ public class DelayedAction {
 	}
 	
 	/**
-	 * Starts the delay of the delayed teleport. The checking for nearby players is done at this point, and after this
-	 * only moving, hitting or taking damage can cancel the teleportation during the wait period
+	 * Starts the delay of the delayed teleport. The checking for nearby players is done at this point, and after this only moving, hitting or taking damage can
+	 * cancel the teleportation during the wait period
 	 */
 	public void start() {
 		MetadataUtils.setFixedMetadata(p, DELAYED_ACTION_META, this, plugin);
