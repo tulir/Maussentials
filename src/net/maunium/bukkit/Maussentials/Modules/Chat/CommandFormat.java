7ebfa3658b20a035b88e7211b8cd2237597d178c
Tulir293 <tulir293@maunium.net>
2015-08-11 17:39:52 +0300

    Implement format command.
    TODO: Add output messages

--- src/net/maunium/bukkit/Maussentials/Modules/Chat/CommandFormat.java
+++ src/net/maunium/bukkit/Maussentials/Modules/Chat/CommandFormat.java
@@ -1,5 +1,8 @@
 package net.maunium.bukkit.Maussentials.Modules.Chat;
 
+import java.sql.SQLException;
+import java.util.UUID;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,8 +20,27 @@ public class CommandFormat implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// TODO Auto-generated method stub
-		return false;
+		if (args.length > 2) {
+			StringBuffer sb = new StringBuffer();
+			for (int i = 2; i < args.length; i++)
+				sb.append(args[i] + " ");
+			sb.deleteCharAt(sb.length() - 1);
+			String format = sb.toString();
+			
+			if (args[0].equalsIgnoreCase("group")) {
+				host.setGroupFormat(args[1], format);
+			} else if (args[0].equalsIgnoreCase("player")) {
+				UUID u;
+				try {
+					u = plugin.getPlayerData().getLatestUUIDByName(args[1]);
+				} catch (SQLException e) {
+					e.printStackTrace();
+					return true;
+				}
+				host.setPlayerFormat(u, format);
+			} else return false;
+			return true;
+		} else return false;
 	}
 	
 }
