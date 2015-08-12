7a57ddaea8074a3c1bcf0c2266306335d427069e
Tulir293 <tulir293@maunium.net>
2015-08-12 08:35:53 +0300

    Documentation

--- src/net/maunium/bukkit/Maussentials/Utils/I18n.java
+++ src/net/maunium/bukkit/Maussentials/Utils/I18n.java
@@ -10,7 +10,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 /**
- * An basic internationalization class that uses files with the Java Properties format. I18n = Internationalization
+ * An basic internationalization class that uses files with the Java Properties format. I18n =
+ * Internationalization
  * 
  * @author Tulir293
  * @since 0.1
@@ -20,12 +21,14 @@ public class I18n {
 	
 	/**
 	 * Creates an Internationalization instance without a name.<br>
-	 * The created instance can only be used through the non-static method {@link #format(node, replace)}
+	 * The created instance can only be used through the non-static method
+	 * {@link #format(node, replace)}
 	 * 
 	 * @param langDir The directory in which language files are stored.
 	 * @param language The name of the language file without the .lang extension
 	 * @throws FileNotFoundException Thrown when the language file could not be found.
-	 * @throws IOException Thrown when there's an error creating the streams or loading the properties.
+	 * @throws IOException Thrown when there's an error creating the streams or loading the
+	 *             properties.
 	 */
 	public static I18n createInstance(File langDir, String language) throws FileNotFoundException, IOException {
 		return new I18n(new File(langDir, language + ".lang"), new File(langDir, "en_US.lang"));
@@ -33,11 +36,13 @@ public class I18n {
 	
 	/**
 	 * Creates an Internationalization instance without a name.<br>
-	 * The created instance can only be used through the non-static method {@link #format(node, replace)}
+	 * The created instance can only be used through the non-static method
+	 * {@link #format(node, replace)}
 	 * 
 	 * @param langFile The language file.
 	 * @throws FileNotFoundException Thrown when the language file could not be found.
-	 * @throws IOException Thrown when there's an error creating the streams or loading the properties.
+	 * @throws IOException Thrown when there's an error creating the streams or loading the
+	 *             properties.
 	 */
 	public static I18n createInstance(File langFile) throws FileNotFoundException, IOException {
 		return new I18n(langFile, new File(langFile.getParentFile(), "en_US.lang"));
@@ -45,13 +50,16 @@ public class I18n {
 	
 	/**
 	 * Creates an Internationalization instance without a name.<br>
-	 * The created instance can only be used through the non-static method {@link #format(node, replace)}
+	 * The created instance can only be used through the non-static method
+	 * {@link #format(node, replace)}
 	 * 
 	 * @param langDir The directory in which language files are stored.
 	 * @param language The name of the language file without the .lang extension
-	 * @param fallback The name of the language to use if the main language couldn't be found. Also without the .lang extension.
+	 * @param fallback The name of the language to use if the main language couldn't be found. Also
+	 *            without the .lang extension.
 	 * @throws FileNotFoundException Thrown when the language file could not be found.
-	 * @throws IOException Thrown when there's an error creating the streams or loading the properties.
+	 * @throws IOException Thrown when there's an error creating the streams or loading the
+	 *             properties.
 	 */
 	public static I18n createInstance(File langDir, String language, String fallback) throws FileNotFoundException, IOException {
 		return new I18n(new File(langDir, language + ".lang"), new File(langDir, fallback + ".lang"));
@@ -59,12 +67,14 @@ public class I18n {
 	
 	/**
 	 * Creates an Internationalization instance without a name.<br>
-	 * The created instance can only be used through the non-static method {@link #format(node, replace)}
+	 * The created instance can only be used through the non-static method
+	 * {@link #format(node, replace)}
 	 * 
 	 * @param langFile The language file.
 	 * @param fallbackFile The file to use if the language file doesn't exist.
 	 * @throws FileNotFoundException Thrown when the language file could not be found.
-	 * @throws IOException Thrown when there's an error creating the streams or loading the properties.
+	 * @throws IOException Thrown when there's an error creating the streams or loading the
+	 *             properties.
 	 */
 	public static I18n createInstance(File langFile, File fallbackFile) throws FileNotFoundException, IOException {
 		return new I18n(langFile, fallbackFile);
@@ -103,32 +113,43 @@ public class I18n {
 	 * @param node The internationalization node.
 	 * @param arguments The arguments.
 	 * @return The value got from the language file with added arguments.<br>
-	 *         If the node couldn't be found, it returns a string with the following format:<br>
-	 *         node (arguments)
+	 *         If the node couldn't be found, it returns null.
 	 */
 	public String translate(String node, Object... arguments) {
 		try {
+			// See if the language map contains the given node.
 			if (lang.containsKey(node)) {
+				// Get the message and parse styles/newlines in it.
 				String rtrn = ChatFormatter.formatAll(lang.getProperty(node)).replace("<br>", "\n");
 				int i = 0;
+				// Loop through the given arguments.
 				for (Object o : arguments) {
+					// If the argument is null, replace it with the String "null".
 					if (o == null) o = "null";
+					// Replace all argument slots which should not be formatted with the
+					// non-formatted argument.
 					rtrn = rtrn.replace("{" + i + "}>>nf", o.toString());
+					// Replace all remaining argument slots with the formatted argument.
 					rtrn = rtrn.replace("{" + i + "}", ChatFormatter.formatAll(o.toString()));
 					i++;
 				}
+				// Return the formatted message.
 				return rtrn;
+				// If the map doesn't contain the node, return null.
 			} else return null;
 		} catch (Throwable t) {
+			// Return null if something went wrong.
 			return null;
 		}
 	}
 	
 	/**
-	 * A basic interface that has a method which is usually used to call the {@link #format(node, arguments)} method of a specific i18n instance. I15r =
+	 * A basic interface that has a method which is usually used to call the
+	 * {@link #translate(node, arguments)} method of a specific i18n instance. I15r =
 	 * Internationalizer
 	 * 
 	 * @author Tulir293
+	 * @since 0.1
 	 */
 	public static interface I15r {
 		public String translate(String node, Object... replace);
